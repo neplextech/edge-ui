6c93a1b973e271ae341bbaa4486fcc5281e04162
skdhg <46562212+skdhg@users.noreply.github.com>
2023-06-05 13:29:54 +0545

    chore: build script update

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -3,13 +3,14 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     dts: true,
     clean: true,
-    entry: ['./src/index.ts', '!./src/*.css', '!./src/stories', '!./src/*.stories.tsx'],
+    entry: ['./src', '!./src/*.css', '!./src/stories', '!./src/*.stories.tsx'],
     skipNodeModulesBundle: true,
-    bundle: true,
+    bundle: false,
     format: ['cjs'],
     keepNames: true,
     minify: false,
     outDir: 'dist',
     target: 'es6',
-    silent: true
+    silent: true,
+    sourcemap: true
 });
